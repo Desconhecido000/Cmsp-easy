1cc929903bc0005affaf668322520591e18f12ae
Desconhecido000 <lubat5664@gmail.com>
2024-11-01 22:03:24 -0300

    script.js

--- script.js
+++ script.js
@@ -7,10 +7,14 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const uf = document.getElementById('uf').value;
     const senha = document.getElementById('senha').value;
 
-    // Simulação de verificação de login
+    // Verifica se todos os campos estão preenchidos
     if (ra && digito && uf && senha) {
-        // Redireciona para a página de tarefas
-        window.location.href = 'tarefas.html';
+        document.getElementById('message').innerText = "Logando na conta...";
+
+        // Redireciona para a página de tarefas após uma pequena espera
+        setTimeout(() => {
+            window.location.href = 'tarefas.html';
+        }, 2000);
     } else {
         document.getElementById('message').innerText = "Por favor, preencha todos os campos.";
     }
